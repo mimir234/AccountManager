d00d087b12288dc9685f6d61e06925e147296ca8
mimir234 <colombetj@gmail.com>
2014-02-17 09:39:21 +0100

    test

--- org.jco.accountManager.ihm.swing/src/main/java/org/jco/accountManager/ihm/swing/Serializator.java
+++ org.jco.accountManager.ihm.swing/src/main/java/org/jco/accountManager/ihm/swing/Serializator.java
@@ -9,11 +9,15 @@ public class Serializator {
 	
 	public static SimpleDateFormat dateFormatter = new SimpleDateFormat("dd/MM/yyyy");
 
-	public static String[] getAccountEventToRow(final IAccountEvent event) {
-		return new String[] {"", getStringFormattedDate(event.getDate()), event.getLabel(), String.valueOf(event.getPay()), "", ""};
+	public static String[] serializeAccountEventToRow(final IAccountEvent event) {
+		return new String[] {"", serializeDateToFormattedString(event.getDate()), event.getLabel(), String.valueOf(event.getPay()), "", ""};
 	}
 	
-	public static String getStringFormattedDate(final Date date){
+	public static String serializeAccountEventToString(final IAccountEvent event) {
+		return new String("["+event.getAccountNumber()+"] ["+event.getUid()+"] ["+serializeDateToFormattedString(event.getDate())+"]["+event.getType()+"] "+event.getPay() +" "+ event.getLabel());
+	}
+	
+	public static String serializeDateToFormattedString(final Date date){
 		return dateFormatter.format(date.getTime());
 	}
 }
